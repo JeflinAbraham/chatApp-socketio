9b11cf9d9036d17b8a81a04f384e11290e919180
JeflinAbraham <jeflinabraham21@gmail.com>
2024-06-19 02:17:40 +0530

    implemented logout functionality on frontend

---
+++ client/src/components/LogoutPopover.jsx
@@ -0,0 +1,64 @@
+import { LogOut } from "lucide-react";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import { toast, useToast } from "./ui/use-toast";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export function LogoutPopover() {
+    const { toast } = useToast();
+    const navigate = useNavigate();
+    const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+    const handleLogout = async () => {
+        try {
+            const response = await axios.post('/api/user/sign-out');
+            if (response.data.success) {
+                toast({
+                    title: 'Success',
+                    description: response.data.message,
+                });
+                navigate('/sign-in');
+            }
+        } catch (error) {
+            toast({
+                title: 'Error',
+                description: error.response.data.message,
+                variant: "destructive"
+            });
+        }
+    };
+
+    return (
+        <Popover open={isPopoverOpen}>
+            <PopoverTrigger asChild>
+                <button onClick={() => setIsPopoverOpen(!isPopoverOpen)}>
+                    <LogOut size={18} />
+                </button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+                <div className="flex flex-col items-center">
+                    <h1>Are you sure you want to logout?</h1>
+                    <div className="flex items-center justify-center gap-x-3">
+                        <button
+                            onClick={handleLogout}
+                            className="bg-blue-600 hover:bg-blue-700 px-2 py-1 rounded-lg text-white mt-2 outline-none"
+                        >
+                            Yes
+                        </button>
+                        <button
+                            onClick={() => setIsPopoverOpen(false)}
+                            className="bg-blue-600 hover:bg-blue-700 px-2 py-1 rounded-lg text-white mt-2 outline-none"
+                        >
+                            No
+                        </button>
+                    </div>
+                </div>
+            </PopoverContent>
+        </Popover>
+    );
+}
--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,15 +1,31 @@
-import { ListFilter, LogOut, MessageSquareDiff, User } from "lucide-react";
-import { Input } from "./ui/input";
+import { LogOut } from 'lucide-react';
 import { AccountPopover } from "./AccountPopover";
+import axios from 'axios';
+import { toast, useToast } from './ui/use-toast';
+import { useNavigate } from 'react-router-dom';
+import { LogoutPopover } from './LogoutPopover';
 
 const Sidebar = () => {
+    const { toast } = useToast();
+    const navigate = useNavigate();
+
     return (
-        <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
-            <div className='bg-slate-100 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between'>
-                <div className='flex flex-col items-center'>
+        <div className='w-full h-full bg-gray-400 flex'>
+            {/* leftmost mini panel */}
+            <div className='bg-blue-500 w-12 h-full py-4 text-white'>
+                <div className='flex flex-col gap-7 items-center justify-center'>
                     <AccountPopover></AccountPopover>
+                    <LogoutPopover></LogoutPopover>
+                    
                 </div>
             </div>
+
+            <div className='bg-slate-100 w-80 h-full py-2 text-slate-600'>
+                <div className='flex items-center justify-center py-2 font-semibold text-2xl border-b border-blue-500'>
+                    <h1>Messages</h1>
+                </div>
+            </div>
+
         </div>
     );
 };
